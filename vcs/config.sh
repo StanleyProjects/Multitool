ae1383797bd911d22aa0a4d5960590e5439a0d68
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-03 21:16:14 +0300

    config

---
+++ vcs/config.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/bash
+
+. checks/require VCS_PAT
+
+GITHUB_USER="$(curl -f 'https://api.github.com/user' -H "Authorization: token $VCS_PAT")"
+. checks/success $? 'Get user error!'
+. checks/filled "$GITHUB_USER" 'User is empty!'
+
+USER_NAME="$(echo "$GITHUB_USER" | yq -erM .name)" || exit 1
+USER_ID="$(echo "$GITHUB_USER" | yq -erM .id)" || exit 1
+USER_LOGIN="$(echo "$GITHUB_USER" | yq -erM .login)" || exit 1
+USER_EMAIL="${USER_ID}+${USER_LOGIN}@users.noreply.github.com"
+
+git config user.name "${USER_NAME}" \
+ && git config user.email "${USER_EMAIL}"
+
+. checks/success $? 'Config error!'
