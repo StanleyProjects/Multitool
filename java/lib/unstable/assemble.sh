55cd29378a586c1eaffc8d27bdf7ea7436c154dd
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-05 21:55:03 +0300

    assemble

--- java/lib/unstable/assemble.sh
+++ java/lib/unstable/assemble.sh
@@ -2,8 +2,26 @@
 
 VARIANT='unstable'
 
-gradle "lib:assemble${VARIANT^}Metadata"
+gradle "lib:assemble${VARIANT^}Metadata" \
+ && gradle "lib:assemble${VARIANT^}MavenMetadata" \
+ && gradle "lib:assemble${VARIANT^}Jar" \
+ && gradle "lib:assemble${VARIANT^}Source"
 
 . $mt/checks/success $? "Assemble \"$VARIANT\" error!"
 
-. $mt/checks/file 'lib/build/yml/metadata.yml'
+ISSUER='lib/build/yml/maven-metadata.yml'
+. $mt/checks/file "${ISSUER}"
+
+ARTIFACT_ID="$(yq -e .repository.artifactId "${ISSUER}")" || exit 1
+VERSION="$(yq -e .version "${ISSUER}")" || exit 1
+
+ISSUER='lib/build/yml/metadata.yml'
+. $mt/checks/file "${ISSUER}"
+
+. $mt/checks/eq "${VERSION}" "$(yq -erM .version "${ISSUER}")" 'Version error!'
+
+ISSUER="lib/build/libs/${ARTIFACT_ID}-${VERSION}.jar"
+. $mt/checks/file "${ISSUER}"
+
+ISSUER="lib/build/libs/${ARTIFACT_ID}-${VERSION}-sources.jar"
+. $mt/checks/file "${ISSUER}"
