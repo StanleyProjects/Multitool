babbdbbece66894af7eaf7ab790e715f9aa2add6
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-10 15:05:41 +0300

    commit

--- vcs/commit.sh
+++ vcs/commit.sh
@@ -8,7 +8,7 @@ COMMIT_TAG="$2"
 . $mt/checks/require COMMIT_MESSAGE COMMIT_TAG
 
 git add . \
- && git commit -m "${COMMIT_MESSAGE}" \
- && git tag "${COMMIT_TAG}"
+ && git commit -S -m "${COMMIT_MESSAGE}" \
+ && git tag -s "${COMMIT_TAG}" -m "${COMMIT_TAG}"
 
 . $mt/checks/success $? "Commit \"${COMMIT_TAG}\" error!"
