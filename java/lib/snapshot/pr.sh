2b4026c315fd574d3b25e6f064b3a29d257dbefd
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-12 21:56:43 +0300

    fix unstable

--- java/lib/snapshot/pr.sh
+++ java/lib/snapshot/pr.sh
@@ -4,9 +4,9 @@
 . $mt/gh/config.sh
 . $mt/vcs/merge.sh
 . $mt/java/lib/snapshot/assemble.sh
-. $mt/java/lib/unstable/commit.sh
+. $mt/java/lib/unstable/commit.sh # todo
 . $mt/java/lib/snapshot/check.sh
 . $mt/gh/push.sh
-. $mt/java/lib/unstable/mvn/snapshot/deploy.sh
+. $mt/java/lib/unstable/mvn/snapshot/deploy.sh # todo
 . $mt/java/lib/unstable/gh/release.sh
 . $mt/java/lib/unstable/message.sh
--- java/lib/unstable/pr.sh
+++ java/lib/unstable/pr.sh
@@ -4,9 +4,9 @@
 . $mt/gh/config.sh
 . $mt/vcs/merge.sh
 . $mt/java/lib/unstable/assemble.sh
-. $mt/java/lib/unstable/commit.sh
+. $mt/java/lib/commit.sh
 . $mt/java/lib/unstable/check.sh
 . $mt/gh/push.sh
-. $mt/java/lib/unstable/mvn/snapshot/deploy.sh
+. $mt/java/lib/unstable/mvn/deploy.sh
 . $mt/java/lib/unstable/gh/release.sh
 . $mt/java/lib/unstable/message.sh
