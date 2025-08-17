9339d97a5fe99c2713aba3e8b9982ca0f6f80541
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-17 15:53:19 +0300

    java lib snapshot

---
+++ src/main/bash/java/lib/snapshot/pr.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+
+. $mt/gh/checkout.sh
+. $mt/gh/config.sh
+. $mt/git/merge.sh
+. $mt/java/lib/snapshot/assemble.sh
+. $mt/java/lib/commit.sh
+. $mt/java/lib/snapshot/check.sh
+
+echo 'Not implemented!'; exit 1 # todo
+
+. $mt/gh/push.sh
+. $mt/java/lib/unstable/mvn/deploy.sh
+. $mt/java/lib/unstable/gh/release.sh
+. $mt/java/lib/unstable/message.sh
