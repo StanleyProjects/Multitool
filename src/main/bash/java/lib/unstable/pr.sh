a530d417c5030cefe77a04828b5afd73467073a2
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-17 15:20:23 +0300

    java lib unstable

--- src/main/bash/java/lib/unstable/pr.sh
+++ src/main/bash/java/lib/unstable/pr.sh
@@ -6,12 +6,9 @@
 
 . $mt/java/lib/unstable/assemble.sh
 . $mt/java/lib/commit.sh
-# . $mt/java/lib/unstable/check.sh # todo
+. $mt/java/lib/unstable/check.sh
 
 . $mt/gh/push.sh
-# . $mt/java/lib/unstable/mvn/deploy.sh # todo
+. $mt/java/lib/unstable/mvn/deploy.sh
 . $mt/java/lib/unstable/gh/release.sh
-
-echo 'Not implemented!'; exit 1 # todo
-
 . $mt/java/lib/unstable/message.sh
