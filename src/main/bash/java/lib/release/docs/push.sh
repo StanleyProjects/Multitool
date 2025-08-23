7f8ef37a1b5cd45b32d5b44c20ff3d0d24fbdfcf
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-23 15:22:15 +0300

    docs push

--- src/main/bash/java/lib/release/docs/push.sh
+++ src/main/bash/java/lib/release/docs/push.sh
@@ -4,6 +4,15 @@
 
 VARIANT='release'
 
+ISSUER='lib/build/yml/metadata.yml'
+. $mt/checks/file.sh "${ISSUER}"
+VERSION="$(yq -erM .version "${ISSUER}")" || exit 1
+
+COMMIT_MESSAGE="docs:${VERSION}"
+COMMIT_TAG="docs/${VERSION}"
+
+. $mt/gh/tag/test.sh "${COMMIT_TAG}"
+
 ISSUER="lib/build/docs/${VARIANT}/index.html"
 . $mt/checks/file.sh "${ISSUER}"
 
@@ -13,14 +22,9 @@ mkdir "${DOCS_PATH}"
 VCS_URL="https://${VCS_PAT}@github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git"
 
 git -C "${DOCS_PATH}" init \
- && git -C "${DOCS_PATH}" remote add origin "${VCS_URL}" \
- && git -C "${DOCS_PATH}" fetch --depth=1 origin 'gh-pages' \
- && git -C "${DOCS_PATH}" checkout 'gh-pages'
+ && git -C "${DOCS_PATH}" remote add origin "${VCS_URL}"
 
-. $mt/checks/success.sh $? 'Checkout docs error!'
-
-if [[ -f "${DOCS_PATH}/docs/${VERSION}/index.html" ]]; then
- echo 'Docs version error!'; exit 1; fi
+. $mt/checks/success.sh $? 'Init docs error!'
 
 ISSUER='.mt/gh-gpg-keys.json'
 . $mt/checks/file.sh "${ISSUER}"
@@ -41,13 +45,44 @@ git -C "${DOCS_PATH}" config user.name "${USER_NAME}" \
 
 . $mt/checks/success.sh $? 'Config docs error!'
 
-ISSUER='lib/build/yml/metadata.yml'
-. $mt/checks/file.sh "${ISSUER}"
-VERSION="$(yq -erM .version "${ISSUER}")" || exit 1
+BRANCH_NAME='gh-pages'
+VCS_API='https://api.github.com'
+REP_URL="${VCS_API}/repos/${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
+VCS_URL="${REP_URL}/branches/${BRANCH_NAME}"
+ISSUER='.mt/gh-branch-pages.json'
+CODE="$(curl -s -w %{http_code} -o "${ISSUER}" "${VCS_URL}")"
+if [ "${CODE}" == 200 ]; then
+ git -C "${DOCS_PATH}" fetch --depth=1 origin "${BRANCH_NAME}" \
+  && git -C "${DOCS_PATH}" checkout "${BRANCH_NAME}"
+ . $mt/checks/success.sh $? 'Checkout pages error!'
+elif [ "${CODE}" == 404 ]; then
+ git -C "${DOCS_PATH}" checkout --orphan "${BRANCH_NAME}"
+ . $mt/checks/success.sh $? "Checkout ${BRANCH_NAME} error!"
+ echo "${REPOSITORY_NAME}" > "${DOCS_PATH}/index.html" \
+  && git -C "${DOCS_PATH}" add . \
+  && git -C "${DOCS_PATH}" commit -m 'init pages'
+ . $mt/checks/success.sh $? 'Commit pages error!'
+ git -C "${DOCS_PATH}" push -u origin "${BRANCH_NAME}"
+ . $mt/checks/success.sh $? "Push ${BRANCH_NAME} error!"
+else
+ echo "Get branch ${BRANCH_NAME} error!"; exit 1; fi
+
+if [[ -f "${DOCS_PATH}/docs/${VERSION}/index.html" ]]; then
+ echo 'Docs version error!'; exit 1; fi
 
 mkdir "${DOCS_PATH}/docs"
 cp -r "lib/build/docs/${VARIANT}" "${DOCS_PATH}/docs/${VERSION}"
 
 . $mt/checks/success.sh $? 'Copy docs error!'
 
-echo 'Not implemented!'; exit 1 # todo
+git -C "${DOCS_PATH}" add . \
+ && git -C "${DOCS_PATH}" commit -S -m "${COMMIT_MESSAGE}" \
+ && git -C "${DOCS_PATH}" tag -s "${COMMIT_TAG}" -m "${COMMIT_MESSAGE}"
+
+. $mt/checks/success.sh $? 'Commit docs error!'
+
+git -C "${DOCS_PATH}" push --follow-tags
+
+. $mt/checks/success.sh $? 'Push docs error!'
+
+echo "https://${REPOSITORY_OWNER}.github.io/${REPOSITORY_NAME}/docs/${VERSION}"
