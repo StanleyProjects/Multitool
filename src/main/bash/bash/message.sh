dc4b43dec70d89c82c149d5886a1f64cde2857b6
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-16 16:13:56 +0300

    fix bash release

--- src/main/bash/bash/message.sh
+++ src/main/bash/bash/message.sh
@@ -30,15 +30,6 @@ MESSAGE+="
 \`*\` [${TARGET_COMMIT::7}](${REP_URL}/commit/${TARGET_COMMIT})
 "
 
-HASHES="build/zip/${REP_NAME}-${VERSION}-hashes.txt"
-. $mt/checks/file.sh "${HASHES}"
-
-MESSAGE+="
-\`\`\`
-$(cat "${HASHES}")
-\`\`\`
-"
-
 MESSAGE+=$'\n'
 MESSAGE+="\`${VERSION}\`"
 
