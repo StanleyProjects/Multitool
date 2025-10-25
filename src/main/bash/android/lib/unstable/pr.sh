4fc1cbe10d62df254c47d7794337e0db8c691bcb
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-25 15:19:00 +0300

    mvn

--- src/main/bash/android/lib/unstable/pr.sh
+++ src/main/bash/android/lib/unstable/pr.sh
@@ -10,10 +10,10 @@
 . $mt/java/lib/commit.sh
 . $mt/android/lib/unstable/check.sh
 
-. $mt/gh/push.sh
+#. $mt/gh/push.sh
+. $mt/android/lib/unstable/mvn/deploy.sh
 
 echo 'Not implemented!'; exit 1 # todo
 
-. $mt/android/lib/unstable/mvn/deploy.sh
 . $mt/android/lib/unstable/gh/release.sh
 . $mt/android/lib/unstable/message.sh
